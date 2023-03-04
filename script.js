72b5017f4462e37502142832942495188c133374
DanishRayeen <danishrayeen32@gmail.com>
2023-03-04 20:23:33 +0530

    DOM Assignment

---
+++ script.js
@@ -0,0 +1,22 @@
+// let body = document.querySelector('body')
+// body = document.querySelector('.wrapper')
+// body = document.querySelector('.archer').childNodes[5].textContent = 'The Aecher'
+
+// body = document.querySelector('.goblin').childNodes[5].textContent = 'The Globin'
+// body = document.querySelector('.clash-card__unit-stats--goblin').style.backgroundColor = '#82BB30'
+// body = document.querySelector('.one-third').style.color = '#ffffff'
+// body = document.querySelector('.clash-card__unit-stats--goblin').style.color = '#ffffff'
+
+// body = document.querySelector
+
+// console.log(body);
+
+function backgroundColor(color) {
+    let body = document.querySelector('body')
+}
+backgroundColor(document.querySelector('.clash-card__unit-stats--barbarian').style.backgroundColor = '#82BB30')
+backgroundColor(document.querySelector('.clearfix').style.color = '#ffffff')
+backgroundColor(document.querySelector('.no-border').style.color = '#ffffff')
+backgroundColor(document.querySelector('.clash-card__unit-stats--archer').style.backgroundColor = 'pink')
+backgroundColor(document.querySelector('.clash-card__unit-stats--archer').style.color = '#ffffff')
+backgroundColor(document.querySelector('.clash-card__unit-stats--archer'))
